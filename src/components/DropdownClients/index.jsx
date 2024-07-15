612b05895c10fac880c46365227155f31e38e66d
Florent Cuer <florentcuer@gmail.com>
2024-07-15 17:58:35 +0100

    changed font size on client list

--- src/components/DropdownClients/index.jsx
+++ src/components/DropdownClients/index.jsx
@@ -6,7 +6,7 @@ export default function DropdownClients({ clients, clientState }) {
     }
 
     return (
-        <select onChange={clientId} className="rounded border h-8 pl-3 bg-white w-32 md:w-auto">
+        <select onChange={clientId} className="text-sm rounded border h-8 pl-3 bg-white w-32 md:w-auto md:text-base">
             <option className="bg-white pl-3" value="">Client List</option>
             {clients.map(client => <option key={client.id} value={client.id}>{client.name}</option>)}
         </select>
