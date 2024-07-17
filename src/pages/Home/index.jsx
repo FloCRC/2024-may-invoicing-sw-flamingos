259754fb2feaa5877679a2bc6d1387409cf4574f
Florent Cuer <florentcuer@gmail.com>
2024-07-17 10:49:31 +0100

    fixed class instead of className on one element

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <InvoiceList />
             <a href="#top">
                 <div onClick={window.scrollTo(0, 0)} className="fixed bottom-5 right-1 text-slate-600 flex md:right-5 hover:opacity-50">
-                    <p><i class="fa-solid fa-angle-up"></i></p>
+                    <p><i className="fa-solid fa-angle-up"></i></p>
                     <p className="hidden pl-1 md:block">Top</p>
                 </div>
             </a>
